017a60c81ae18cea390ec8f7e12ba7f3de87ebdc
symonogwe <symonogwe@gmail.com>
2023-08-07 15:28:30 +0300

    Implement myDisplayController Module
    - Add methods playerOneWins, playerTwoWins & noPlayerWins methods which
    are executed by the myGame.gameOver method depending on the result of
    the game

--- index.js
+++ index.js
@@ -38,148 +38,217 @@ const myGame = (function() {
         _playerArray[1] : _playerArray[0];
     }
 
-    function _printPlayer() {
-        console.log(`It is ${_currentPlayer.marker}'s turn`);
+    function printPlayer() {
+       return `it is ${_currentPlayer.marker}'s turn`;
     }
 
     function playRound(index) {
         _currentPlayer.addToBoard(index);
         switchPlayer();
-        _printPlayer();
-        _gameOver();
+        gameOver();
     }
 
-    function _gameOver() {
-        // player one wins
+    function gameOver() {
+        // CHECK IF PLAYER ONE WINS
         if (myGameBoard.render()[0] === player1.marker &&
         myGameBoard.render()[1] === player1.marker &&
         myGameBoard.render()[2] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         if (myGameBoard.render()[3] === player1.marker &&
         myGameBoard.render()[4] === player1.marker &&
         myGameBoard.render()[5] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         if (myGameBoard.render()[6] === player1.marker &&
         myGameBoard.render()[7] === player1.marker &&
         myGameBoard.render()[8] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         if (myGameBoard.render()[0] === player1.marker &&
         myGameBoard.render()[3] === player1.marker &&
         myGameBoard.render()[6] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         if (myGameBoard.render()[1] === player1.marker &&
         myGameBoard.render()[4] === player1.marker &&
         myGameBoard.render()[7] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         if (myGameBoard.render()[2] === player1.marker &&
         myGameBoard.render()[5] === player1.marker &&
         myGameBoard.render()[8] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         if (myGameBoard.render()[6] === player1.marker &&
         myGameBoard.render()[4] === player1.marker &&
         myGameBoard.render()[2] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         if (myGameBoard.render()[0] === player1.marker &&
         myGameBoard.render()[4] === player1.marker &&
         myGameBoard.render()[8] === player1.marker) {
-            console.log("PLAYER 1 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerOneWins();
         }
         // player 2 wins
         if (myGameBoard.render()[0] === player2.marker &&
         myGameBoard.render()[1] === player2.marker &&
         myGameBoard.render()[2] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         if (myGameBoard.render()[3] === player2.marker &&
         myGameBoard.render()[4] === player2.marker &&
         myGameBoard.render()[5] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         if (myGameBoard.render()[6] === player2.marker &&
         myGameBoard.render()[7] === player2.marker &&
         myGameBoard.render()[8] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         if (myGameBoard.render()[0] === player2.marker &&
         myGameBoard.render()[3] === player2.marker &&
         myGameBoard.render()[6] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         if (myGameBoard.render()[1] === player2.marker &&
         myGameBoard.render()[4] === player2.marker &&
         myGameBoard.render()[7] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         if (myGameBoard.render()[2] === player2.marker &&
         myGameBoard.render()[5] === player2.marker &&
         myGameBoard.render()[8] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         if (myGameBoard.render()[6] === player2.marker &&
         myGameBoard.render()[4] === player2.marker &&
         myGameBoard.render()[2] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         if (myGameBoard.render()[0] === player2.marker &&
         myGameBoard.render()[4] === player2.marker &&
         myGameBoard.render()[8] === player2.marker) {
-            console.log("PLAYER 2 WINS")
             myGameBoard.render().length = 0;
             switchPlayer();
+            myDisplayController.playerTwoWins();
         }
         // check for a draw
         if (myGameBoard.render().length === 9 && 
         myGameBoard.render().every(i => !i.includes(" "))) {
-            console.log("ITS A DRAW")
             myGameBoard.render().length = 0;
-            _currentPlayer = _playerArray[0];
+            switchPlayer();
+            myDisplayController.noPlayerWins();
         }
         
 
     }
     
 
-    return {playRound}
+    return {playRound, printPlayer, gameOver}
     
 })();
 
+// SCREEN CONTROLLER MODULE
+const myDisplayController = (function() {
+
+    // DOM elements
+    const _gameContainer = document.querySelector(".game-grid-container");
+    const _playerTurnDisplay = document.querySelector(".player-turn");
+
+    // methods that update display on the result of the game
+    function playerOneWins() {
+        _playerTurnDisplay.textContent = "Game Over! X wins";
+        
+        console.log(myGame.playerTurn);
+    }
+
+    function playerTwoWins() {
+        _playerTurnDisplay.textContent = "Game Over! O wins";
+
+        console.log(myGame.playerTurn);
+    }
+
+    function noPlayerWins() {
+        _playerTurnDisplay.textContent = "Its a Tie!"
+    }
+
+    // Method that updates screen after EACH player plays a turn
+    function updateScreen() {
+        _gameContainer.textContent = "";
+
+        // creates divs based on length of gameBoard array
+        for (let i = 0; i < myGameBoard.render().length; i++) {
+            let gameDiv = document.createElement("div");
+            gameDiv.classList.add("game-grid");
+            gameDiv.addEventListener("click", clickHandler);
+            gameDiv.dataset.index = i;
+
+            // Place player's marker on the clicked div
+            if (myGameBoard.render()[i] === "x") {
+                let divP = document.createElement("p");
+                divP.textContent = "X";
+                divP.classList.add("player-marker");
+                gameDiv.append(divP);
+            }else if (myGameBoard.render()[i] === "o") {
+                let divP = document.createElement("p");
+                divP.textContent = "O";
+                divP.classList.add("player-marker");
+                gameDiv.append(divP);
+            }
+            
+            _playerTurnDisplay.textContent = myGame.printPlayer();
+            
+            
+            _gameContainer.append(gameDiv);
+            
+        }
+        
+        console.log(_gameContainer);
+    }
+
+    // click event function added to all created box divs
+    function clickHandler(e) {
+        myGame.playRound(e.target.dataset.index);
+        console.log(myGameBoard.render());
+        updateScreen()
+    }
+    
+    return {updateScreen, playerOneWins, playerTwoWins, noPlayerWins}
+
+})();
+
+myDisplayController.updateScreen()
 
 console.log(myGameBoard.render());
 
